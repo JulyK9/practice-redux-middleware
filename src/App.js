08af4388775d0851c4f4983ca0b1350a5c608fb1
JKKIM <jkesonjk@gmail.com>
2023-08-09 18:48:01 +0900

    feat: todos 상태접근 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { increaseCounter } from './redux/counterReducer';
+import { getTodo } from './redux/todoReducer';
 
 function App() {
   const count = useSelector((store) => store.counter);
+
   const dispatch = useDispatch();
 
   // 디스패치를 통해 액션을 스토어에 전달(feat. 액션크리에이터)
@@ -12,11 +14,26 @@ function App() {
     dispatch(increaseAction); // 디스패치로 액션 전달
   };
 
+  const todos = useSelector((store) => store.todo.todos);
+
+  // const handleGetTodos = () => {
+  //   const getTodoAction = getTodo(todos);
+  //   dispatch(getTodoAction);
+  // };
+
   return (
     <div className="App">
       <h2>Practice Redux Middleware</h2>
       <h3>count: {count}</h3>
       <button onClick={handleIncreaseNumber}>increase</button>
+      <button onClick={() => {}}>get Todos</button>
+      <section>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }

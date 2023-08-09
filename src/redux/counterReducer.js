f680015d024813afe5772f0799f61c86ab5dc69e
JKKIM <jkesonjk@gmail.com>
2023-08-09 15:50:33 +0900

    feat: 리듀서 액션 타입 상수화 및 수정, 액션 크리에이터 생성

--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -1,7 +1,19 @@
+// 액션 타입 상수화
+const INCREASE = 'counter/increase';
+
+// 액션 크리에이터 설정(optional)
+// 전달할 액션 타입을 함수화하여 실행시 타입을 반환, 나중에 필요한 곳에서 디스패치로 엑션 전달시 활용
+export const increaseCounter = () => {
+  return {
+    type: INCREASE,
+  };
+};
+
+// 초기상태 직관적 설정
 const INITIAL_STATE = 0;
 
 const counterReducer = (prevState = INITIAL_STATE, action) => {
-  if (action.type === 'counter/increase') {
+  if (action.type === INCREASE) {
     return prevState + 1;
   }
 

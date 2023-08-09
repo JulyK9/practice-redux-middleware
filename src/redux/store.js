e93e95950566d620a819179e58a7096b6f2d6d48
JKKIM <jkesonjk@gmail.com>
2023-08-09 16:00:06 +0900

    feat: 루트 리듀서 설정 및 store 설정

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { combineReducers, createStore } from 'redux';
+import counterReducer from './counterReducer';
+
+// combineReducers를 통해 슬라이스된 리듀서로부터 루트 리듀서 만들어 combine 해주기
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+// store를 만들고 리듀서를 store에 넣어주기
+// deprecated 됐으나 pure redux 로직을 통한 흐름을 이해하기 위해 먼저 사용해보고
+// toolkit에서 지원하는 configureStore로 변환할 것
+const store = createStore(rootReducer);
+
+export default store;

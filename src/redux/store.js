5ec88c246dbd0214dc1aba433fe59a4c3c80b306
JKKIM <jkesonjk@gmail.com>
2023-08-09 19:41:12 +0900

    feat: store에서 적용할 미들웨어 적용 설정

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import counterReducer from './counterReducer';
 import todoReducer from './todoReducer';
+import { thunk } from './middleware/thunk';
+import { logger } from './middleware/loger';
 
 // combineReducers를 통해 슬라이스된 리듀서로부터 루트 리듀서 만들어 combine 해주기
 const rootReducer = combineReducers({
@@ -11,6 +13,6 @@ const rootReducer = combineReducers({
 // store를 만들고 리듀서를 store에 넣어주기
 // deprecated 됐으나 pure redux 로직을 통한 흐름을 이해하기 위해 먼저 사용해보고
 // toolkit에서 지원하는 configureStore로 변환할 것
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 export default store;

bdbcd70c68b95475b24de1fbbde4c2768b16c3c8
JKKIM <jkesonjk@gmail.com>
2023-08-09 16:12:43 +0900

    feat: ui 설정 및 상태 및 디스패치 연결 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { increaseCounter } from './redux/counterReducer';
 
 function App() {
-  return <div className="App">Practice Redux Middleware</div>;
+  const count = useSelector((store) => store.counter);
+  const dispatch = useDispatch();
+
+  // 디스패치를 통해 액션을 스토어에 전달(feat. 액션크리에이터)
+  const handleIncreaseNumber = () => {
+    const increaseAction = increaseCounter(); // 액션크리에이터로 액션 생성
+    dispatch(increaseAction); // 디스패치로 액션 전달
+  };
+
+  return (
+    <div className="App">
+      <h2>Practice Redux Middleware</h2>
+      <h3>count: {count}</h3>
+      <button onClick={handleIncreaseNumber}>increase</button>
+    </div>
+  );
 }
 
 export default App;

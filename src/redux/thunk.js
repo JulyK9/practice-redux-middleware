ac65f545dc55df812dbba12baa1d331d5a7aae38
JKKIM <jkesonjk@gmail.com>
2023-08-09 19:30:54 +0900

    feat: api 호출을 리덕스로 처리하기 위한 미들웨어 thunk 설정

---
+++ src/redux/thunk.js
@@ -0,0 +1,6 @@
+// 미들웨어 생성: api를 호출하는 부분에 대한 처리를 thunk 에서 처리
+export const thunk = (store) => (next) => (action) => {
+  typeof action === 'function' // action이 (객체 타입이 아니라) 함수이면
+    ? action(store.dispatch, store.getState) // 그 함수를 호출하고
+    : next(action); // 아니면 그 다음으로 넘기기
+};
